8b755f6226329015b108ec59e2b2f43953c55f2d
johnpospisil <audiofreak7@gmail.com>
2019-05-13 10:36:06 -0700

    Re-use the ApprovalCard component fir ither Yes/No user input

--- src/index.js
+++ src/index.js
@@ -19,6 +19,16 @@ const App = () => {
       <ApprovalCard>
         <CommentDetail author="Norbert" timeAgo="Today at 4:00PM" postText="I approve, good sir!" avatar={faker.image.avatar()} />
       </ApprovalCard>
+      <h2>The ApprovaCard can now be re-used for other purposes!</h2>
+      <ApprovalCard >
+        Are you sure you want to do that?
+      </ApprovalCard>
+      <ApprovalCard >
+        <div>
+          <h4>Alert!</h4>
+          This action is undoable. Are you sure you want to continue?
+        </div>
+      </ApprovalCard>
     </div>
   );
 };
