c70f088a85772535e13ec8555ee0cd30a8a6e652
johnpospisil <audiofreak7@gmail.com>
2019-05-13 10:29:23 -0700

    place the comment inside the ApprovalCard

--- src/index.js
+++ src/index.js
@@ -7,11 +7,18 @@ import ApprovalCard from './ApprovalCard';
 const App = () => {
   return (
     <div className="ui container comments">
-      <ApprovalCard />
-      <CommentDetail author="Sam" timeAgo="Today at 1:00PM" postText="Nice post!" avatar={faker.image.avatar()} />
-      <CommentDetail author="Bob" timeAgo="Today at 2:00PM" postText="I like your taste!" avatar={faker.image.avatar()} />
-      <CommentDetail author="Janet" timeAgo="Today at 3:00PM" postText="Good, but strange." avatar={faker.image.avatar()} />
-      <CommentDetail author="Norbert" timeAgo="Today at 4:00PM" postText="I approve, good sir!" avatar={faker.image.avatar()} />
+      <ApprovalCard >
+        <CommentDetail author="Sam" timeAgo="Today at 1:00PM" postText="Nice post!" avatar={faker.image.avatar()} />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail author="Bob" timeAgo="Today at 2:00PM" postText="I like your taste!" avatar={faker.image.avatar()} />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail author="Janet" timeAgo="Today at 3:00PM" postText="Good, but strange." avatar={faker.image.avatar()} />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail author="Norbert" timeAgo="Today at 4:00PM" postText="I approve, good sir!" avatar={faker.image.avatar()} />
+      </ApprovalCard>
     </div>
   );
 };
