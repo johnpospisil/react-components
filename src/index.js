a5c81922fe194b3f5ecaed869b8f52d51522b105
johnpospisil <audiofreak7@gmail.com>
2019-05-13 10:18:03 -0700

    Add capability for and Approve/Decline card

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import faker from 'faker';
 import CommentDetail from './CommentDetail';
+import ApprovalCard from './ApprovalCard';
 
 const App = () => {
   return (
     <div className="ui container comments">
+      <ApprovalCard />
       <CommentDetail author="Sam" timeAgo="Today at 1:00PM" postText="Nice post!" avatar={faker.image.avatar()} />
       <CommentDetail author="Bob" timeAgo="Today at 2:00PM" postText="I like your taste!" avatar={faker.image.avatar()} />
       <CommentDetail author="Janet" timeAgo="Today at 3:00PM" postText="Good, but strange." avatar={faker.image.avatar()} />
