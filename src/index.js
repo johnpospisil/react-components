ec922ed44730711f457609510bfc9eab19e6e9fd
johnpospisil <audiofreak7@gmail.com>
2020-10-04 13:23:27 -0700

    Update CommentDetail to display faker.js data

--- src/index.js
+++ src/index.js
@@ -1,29 +1,49 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import faker from 'faker';
-import CommentDetail from './CommentDetail';
-import ApprovalCard from './ApprovalCard';
+import React from "react";
+import ReactDOM from "react-dom";
+import faker from "faker";
+import CommentDetail from "./CommentDetail";
+import ApprovalCard from "./ApprovalCard";
 
 const App = () => {
   return (
     <div className="ui container comments">
-      <ApprovalCard >
-        <CommentDetail author="Sam" timeAgo="Today at 1:00PM" postText="Nice post!" avatar={faker.image.avatar()} />
+      <ApprovalCard>
+        <CommentDetail
+          author={faker.name.firstName()}
+          timeAgo="Today at 1:00PM"
+          postText={faker.lorem.sentence()}
+          avatar={faker.image.avatar()}
+        />
       </ApprovalCard>
       <ApprovalCard>
-        <CommentDetail author="Bob" timeAgo="Today at 2:00PM" postText="I like your taste!" avatar={faker.image.avatar()} />
+        <CommentDetail
+          author={faker.name.firstName()}
+          timeAgo="Today at 2:00PM"
+          postText={faker.lorem.sentence()}
+          avatar={faker.image.avatar()}
+        />
       </ApprovalCard>
       <ApprovalCard>
-        <CommentDetail author="Janet" timeAgo="Today at 3:00PM" postText="Good, but strange." avatar={faker.image.avatar()} />
+        <CommentDetail
+          author={faker.name.firstName()}
+          timeAgo="Today at 3:00PM"
+          postText={faker.lorem.sentence()}
+          avatar={faker.image.avatar()}
+        />
       </ApprovalCard>
       <ApprovalCard>
-        <CommentDetail author="Norbert" timeAgo="Today at 4:00PM" postText="I approve, good sir!" avatar={faker.image.avatar()} />
+        <CommentDetail
+          author={faker.name.firstName()}
+          timeAgo="Today at 4:00PM"
+          postText={faker.lorem.sentence()}
+          avatar={faker.image.avatar()}
+        />
       </ApprovalCard>
+
       <h2>The ApprovaCard can now be re-used for other purposes!</h2>
-      <ApprovalCard >
-        Are you sure you want to do that?
-      </ApprovalCard>
-      <ApprovalCard >
+      <ApprovalCard>Are you sure you want to do that?</ApprovalCard>
+
+      <ApprovalCard>
         <div>
           <h4>Alert!</h4>
           This action is undoable. Are you sure you want to continue?
@@ -33,4 +53,4 @@ const App = () => {
   );
 };
 
-ReactDOM.render(<App />, document.querySelector('#root'))
+ReactDOM.render(<App />, document.querySelector("#root"));
